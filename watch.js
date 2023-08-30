48cec2d4a1429191cf04798c5c5bae548b33a191
sadid56 <sadidhasan77@gmail.com>
2023-08-30 22:06:36 +0600

    added start button disabled

--- watch.js
+++ watch.js
@@ -4,12 +4,24 @@ const startWatch = ()=>{
        const result = ++sum;
        document.getElementById('watch-timer').innerText = result;
     },1000)
+    const startWatchBtbn = document.getElementById('strat-btn');
+    startWatchBtbn.setAttribute('disabled',true)
+    startWatchBtbn.style.backgroundColor = 'gray';
+    startWatchBtbn.style.cursor = 'not-allowed'
 }
 const stopWatch = ()=>{
     clearInterval(setWatchId)
+    const startWatchBtbn = document.getElementById('strat-btn');
+    startWatchBtbn.removeAttribute('disabled',true)
+    startWatchBtbn.style.backgroundColor = 'blue';
+    startWatchBtbn.style.cursor = 'pointer'
 }
 const clearWatch = ()=>{
     sum = 0;
     document.getElementById('watch-timer').innerText ='0';
     clearInterval(setWatchId)
+    const startWatchBtbn = document.getElementById('strat-btn');
+    startWatchBtbn.removeAttribute('disabled',true)
+    startWatchBtbn.style.backgroundColor = 'blue';
+    startWatchBtbn.style.cursor = 'pointer'
 }
